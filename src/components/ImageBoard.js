8b95e1881282d2498d8431d005e235aea1062453
Evelina <evelinawahlstrom18@gmail.com>
2020-01-03 12:52:30 +0100

    ImageBoard-/images

---
+++ src/components/ImageBoard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ImageBoard(props) {
+  if (!props.images) return "Loading";
+  console.log(props.images, "what is props.images??")
+  return (
+    <div className="list">
+      {props.images.map(image => {
+        return (
+          <img src={image.picture} width="200" height="200" alt="image-pic" />
+        );
+      })}
+    </div>
+  );
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux"
 import auth from './auth'
+import images from './images'
 
 export default combineReducers({
     auth,
+    images
  })
