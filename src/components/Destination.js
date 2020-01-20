274d612900a04b1db1080c6b8fb4cbf80d919e0b
Evelina <evelinawahlstrom18@gmail.com>
2020-01-20 11:08:09 +0100

    testing

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { getWebcamTravel } from '../actions/api'
+var request = require("request");
 
 class Destination extends Component {
-    componentDidMount() {
-        this.props.getWebcamTravel();
-      }
+    onClick = (event) => {
+        event.preventDefault()
+        console.log("try to login w: ", this.state.email ,"and", this.state.password)
+        this.props.login(this.state.email, this.state.password);
+        request
+        .post(`${url}/login`)
+        .send({
+            email: this.state.email,
+            password: this.state.password
+        })
+        .then(result => {
+            console.log("please let it be a token!", result.body)   
+        this.props.login(result.body.jwt)
+        }).catch(err => console.log(err))
+        this.setState({
+            email: "",
+            password: ""
+        })
+    }
     render() {
         return (
             <div>
-               hey 
+               
             </div>
         )
     }
